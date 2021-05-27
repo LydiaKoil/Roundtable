ba77cf2d2b76f5abc037d54bec814218ec821e8c
Daniel Paul <im@danielpaul.me>
2021-05-27 16:04:46 +0100

    working

--- db/migrate/20210526154253_add_replies_to_arguments.rb
+++ db/migrate/20210526154253_add_replies_to_arguments.rb
@@ -1,14 +1,13 @@
 class AddRepliesToArguments < ActiveRecord::Migration[6.1]
   def up
     change_table :arguments do |t|
-      t.references :claim, foreign_key: { to_table: :arguments }
+      t.references :argument, index: true
     end
   end
 
   def down
     change_table :arguments do |t|
-      t.remove_references :claim, foreign_key: { to_table: :arguments }
+      t.remove_references :argument
     end
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2021_05_26_154253) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "type"
-    t.integer "claim_id"
-    t.index ["claim_id"], name: "index_arguments_on_claim_id"
+    t.integer "reply_type"
+    t.integer "argument_id"
+    t.index ["argument_id"], name: "index_arguments_on_argument_id"
     t.index ["user_id"], name: "index_arguments_on_user_id"
   end
 
@@ -36,6 +36,5 @@ ActiveRecord::Schema.define(version: 2021_05_26_154253) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "arguments", "arguments", column: "claim_id"
   add_foreign_key "arguments", "users"
 end
