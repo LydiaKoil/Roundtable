70d55f79a8c1cbf58b07fd0df71712e596a43e2c
Lydtk <47219609+Lydtk@users.noreply.github.com>
2021-06-03 14:37:32 +0100

    Remove show button, shorter tests

--- spec/features/view_arguments.rb
+++ spec/features/view_arguments.rb
@@ -2,48 +2,34 @@ require 'rails_helper'
 
 describe 'Viewing Arguments', type: :feature   do 
   before :each do
+    @argument_1 = argument_with_pros_cons
   end
 
-  it "displays index with Argument titles " do
-    @argument_1 =FactoryBot.create(:argument)
-	
+  it "displays index of arguments" do
+    #@argument_1 =FactoryBot.create(:argument)
   	visit arguments_path
   	expect(page).to have_content @argument_1.title
     expect(page).to have_content @argument_1.user.email.split('@')[0]
-    expect(page).to have_content "Show"
 
   end
 
-  it "displays individual arguments" do
-    @argument_1 =FactoryBot.create(:argument)
+  it "displays individual argument" do
+    #@argument_1 =FactoryBot.create(:argument)
     visit arguments_path
-    click_on "Show"
+    click_link @argument_1.title
     expect(current_path).to eq("/arguments/#{@argument_1.id}")
     expect(page).to have_content @argument_1.title
     expect(page).to have_content @argument_1.body
     expect(page).to have_content @argument_1.user.email.split('@')[0]
-    expect(page).to have_content @argument_1.user.email.split('@')[0] 
+    @argument_1.arguments.pro.each do |arg|
+      expect(page).to have_content arg.title
+    end
+    @argument_1.arguments.con.each do |arg|
+      expect(page).to have_content arg.title
+    end
       
   end
-
-  it "displays pros and cons" do
-    @argument2 = argument_with_pros_cons
-    print @argument2.id
-    print @argument2.title
-    visit arguments_path
-    click_on "Show"
-    expect(current_path).to eq("/arguments/#{@argument2.id}")
-    expect(page).to have_content @argument2.title
-    @argument2.arguments.pro.each do |arg|
-      expect(page).to have_content arg.title
-    end
-    @argument2.arguments.con.each do |arg|
-      expect(page).to have_content arg.title
-    end
-    puts 
-
-  end
-
+  
 
 =begin
   it "displays argument when show clicked" do
