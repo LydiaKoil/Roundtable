79f0f109e0942d35e4f2bb510999bd0aa9e29e63
Lydtk <47219609+Lydtk@users.noreply.github.com>
2021-06-07 10:15:14 +0100

    Refactoring with Shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,30 +2,34 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  subject {User.create(
-  first_name: 'Jane',
-  last_name: 'Doe',
-  email: 'test1@example.com', 
-  password: 'password123', 
-  password_confirmation: 'password123')
-  }
+  # subject {User.create(
+  # first_name: 'Jane',
+  # last_name: 'Doe',
+  # email: 'test1@example.com', 
+  # password: 'password123', 
+  # password_confirmation: 'password123')
+  # }
   
-  it "is valid with valid attributes" do
-    expect(subject).to be_valid
-  end
+  # it "is valid with valid attributes" do
+  #   expect(subject).to be_valid
+  # end
 
-  it "is not valid without an email"do
-    subject.email = nil
-    expect(subject).to_not be_valid
-  end
-  
-
-  it "is not valid without a password"do
-    subject.password = nil
-    expect(subject).to_not be_valid
-  end
-
+  # it "is not valid without an email"do
+  #   subject.email = nil
+  #   expect(subject).to_not be_valid
+  # end
   
 
+  # it "is not valid without a password"do
+  #   subject.password = nil
+  #   expect(subject).to_not be_valid
+  # end
+
+  
+  subject { create(:user) }
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
 
 end
