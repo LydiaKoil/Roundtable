283c3a123cc265e06f3a00b716800dec7cb94358
Lydtk <47219609+Lydtk@users.noreply.github.com>
2021-06-07 14:18:54 +0100

    delete argument fixed

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
  
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "www.example.com"
+  config.hosts << "127.0.0.1"
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
