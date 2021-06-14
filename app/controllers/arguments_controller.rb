4a1fedc3b13bd3dd3f10376ec0e39d27aaa2f22d
LydiaKoil <47219609+LydiaKoil@users.noreply.github.com>
2021-06-14 08:01:47 +0100

    routing changed

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -34,7 +34,11 @@ class ArgumentsController < ApplicationController
     @argument = current_user.arguments.new(argument_params)
     if @argument.save
       @argument.send_reply_notification!
-      redirect_to @argument, notice: 'Argument was successfully created.'        
+      if @argument.argument_id == nil
+        redirect_to arguments_path, notice: 'Argument was successfully created.'
+      else
+        redirect_to Argument.find(@argument.argument_id), notice: 'Argument was successfully created.'
+      end     
     else
       render :new, status: :unprocessable_entity
     end
